333831cfc10738dc9550f0ba23ea1e00f8252019
isurusajith68 <isurusajith68@gmail.com>
2023-05-16 20:33:46 +0530

    styles.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1>React 18 Alpha</h1>
     </div>
   );
 }
