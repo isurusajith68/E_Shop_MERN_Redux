cbb5c6a02196d34de05eef5e5cf25ed587e1c3f7
isurusajith68 <isurusajith68@gmail.com>
2023-05-18 11:28:01 +0530

    getuser funtion

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
-import {LoginPage} from "./Routes.js"
+import { LoginPage } from "./Routes.js";
 import { SignUpPage } from "./Routes.js";
-import {ActivationPage} from "./Routes.js"
-import { ToastContainer } from "react-toastify";
- function App() {
+import { ActivationPage } from "./Routes.js";
+import { ToastContainer, toast } from "react-toastify";
+import axios from "axios";
+import { server } from "./server";
+function App() {
+  useEffect(() => {
+    axios
+      .get(`${server}/user/getuser`, { withCredentials: true })
+      .then((res) => {
+        toast.success(res.data.message);
+      })
+      .catch((err) => {
+        toast.error(err.response.data.message);
+      });
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,5 +1,21 @@
 const ErrorHandler = require('../util/ErrorHandler');
 const catchAsyncErrors = require('./catchAsyncErrors'); 
 const jwt = require('jsonwebtoken');
-
+const User = require('../model/user');
 // Check if user is authenticated or not
+exports.isAuthenticated = catchAsyncErrors(async (req, res, next) => {
+  const { token } = req.cookies;
+ // console.log(token)
+
+  if (!token) {
+    return next(new ErrorHandler("Please login to continue", 401));
+  }
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+  req.user = await User.findById(decoded.id);
+
+  //console.log(decoded.id)
+
+  next();
+});
